9a7d6582c9c6e7c98bf7d99e9f9b9881f1905ac8
zevonjunior <casillas.adrian@googlemail.com>
2024-06-21 19:18:09 +0100

    added form, field validation and testing packages

--- vscode-react-ai-exercises/src/Register.jsx
+++ vscode-react-ai-exercises/src/Register.jsx
@@ -1,8 +1,69 @@
-
+import  {useState} from 'react';
+import './App.css';
 function Register() {
+
+
+const [username, setUsername] = useState('');
+const [mobile, setMobile] = useState('');
+// 2 state items for  username and mobile error
+const [usernameError, setUsernameError] = useState('');
+const [mobileError, setMobileError] = useState('');
+
+
   return (
     <div>
       <h1>Register</h1>
+      <form>
+        <label>
+          Username:
+          <input
+           type="text"
+           name="username"
+           value={username}
+           onChange={e => {
+            const uservalue = e.target.value;
+            setUsername(uservalue);
+            // validate that the username is at least 8 characters long, with at least one lowercase letter, one uppercase character, 1 number and 1 special character (e.g. !@#$%^&*) regex
+            const userRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+            if (!userRegex.test(uservalue)) {
+              console.log('Username is invalid');
+              setUsernameError('Bad user name');
+            } else {
+              console.log('Username is valid');
+              setUsernameError('');
+            }
+          }}
+             />
+          { usernameError && <p style={{color: "red"}}>{usernameError}</p> }
+        </label>
+        <label>
+          UK Mobile Number:
+          <input
+            type="tel"
+            name="mobile"
+            value={mobile}
+            onChange={e => {
+              const mobilevalue = e.target.value;
+              setMobile(mobilevalue);
+              // validate that the mobile number is a UK number (starts with 07 and is 11 digits long) regex
+              const mobileRegex = /^07\d{9}$/;
+              if (!mobileRegex.test(mobilevalue)) {
+                console.log('Mobile number is invalid');
+                setMobileError('Wrong number, dude!');
+              } else {
+                console.log('');
+                setMobileError('');
+              }
+
+
+
+            }}
+            />
+          { mobileError && <p style={{color: "hotpink"}}>{mobileError}</p> }
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
