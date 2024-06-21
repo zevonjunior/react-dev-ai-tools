dcb26793621572fb06ab4d5b0062bf30b5ccf9ed
Y-Wilson2021 <91729373+Y-Wilson2021@users.noreply.github.com>
2024-06-21 13:46:26 +0100

    init react-ai-tools

---
+++ vscode-react-ai-exercises/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App.jsx'
+import Register from './Register.jsx' // Import the RegisterPage component
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+)
+
